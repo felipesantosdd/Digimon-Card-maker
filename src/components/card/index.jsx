e474d768a3874f84cbacb8391d1f46238adb58ed
Felipe <felipesantosdd@gmail.com>
2023-06-15 20:13:41 -0300

    fix: Correção da posição de efeito de herança de digi-tama

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -491,7 +491,7 @@ export function CardComponent() {
                         maxLineWidth = 300
                         cardsoureeffectY = 582
                     } else if (cardValue.type === "Digi-Egg") {
-                        cardsoureeffectY = 580
+                        cardsoureeffectY = 550
                     }
 
 
